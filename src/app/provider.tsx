5fe485e4219e02f46e1ca93c71f64fc65d82e036
Ashwani Singh <singhashwanikumar793@gmail.com>
2024-05-06 13:22:43 +0530

    updated provider file

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,22 +1,31 @@
 "use client";
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect, ReactNode } from "react";
 import { FormDataType } from "./(auth)/signup/page";
 
-// createContext is not supported in Server Components
-export const UserContext = createContext<FormDataType | null>(null);
+export interface UserContextType {
+  loggedInUser: FormDataType | null;
+  setLoggedInUser: React.Dispatch<React.SetStateAction<FormDataType | null>>;
+}
 
-export default function ThemeProvider({ children }) {
+export const UserContext = createContext<UserContextType | null>(null);
+
+interface ThemeProviderProps {
+  children: ReactNode;
+}
+
+export default function ThemeProvider({ children }: ThemeProviderProps) {
   const [loggedInUser, setLoggedInUser] = useState<FormDataType | null>(null);
 
   useEffect(() => {
-    // Retrieve the logged-in user from local storage when the component mounts
     const userFromLocalStorage = localStorage.getItem("loggedInUser");
     if (userFromLocalStorage) {
       setLoggedInUser(JSON.parse(userFromLocalStorage));
     }
-  }, []); // Empty dependency array ensures the effect runs only once when the component mounts
+  }, []);
 
   return (
-    <UserContext.Provider value={loggedInUser}>{children}</UserContext.Provider>
+    <UserContext.Provider value={{ loggedInUser, setLoggedInUser }}>
+      {children}
+    </UserContext.Provider>
   );
 }
