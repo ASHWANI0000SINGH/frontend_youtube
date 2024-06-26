873d37cfa0471a4756fe7e4abd3c04436ffafc86
Ashwani Singh <singhashwanikumar793@gmail.com>
2024-06-26 23:05:52 +0530

    token expiry route to login

--- src/components/UseAuth.tsx
+++ src/components/UseAuth.tsx
@@ -1,7 +1,11 @@
+"use client";
+import { useRouter } from "next/navigation";
+
 import React, { useEffect, useState } from "react";
 
 const UseAuth = () => {
 	const [allowuser, setAllowUser] = useState(false);
+	const router = useRouter();
 
 	useEffect(() => {
 		const token = localStorage.getItem("accessToken");
@@ -10,19 +14,25 @@ const UseAuth = () => {
 			if (!token) {
 				// Token is not provided
 				setAllowUser(false);
+
 				return true;
 			}
 
 			const decodedToken = JSON.parse(atob(token.split(".")[1])); // Decode token payload
 			const expirationTime = decodedToken.exp * 1000; // Expiration time in milliseconds
 			const currentTime = Date.now(); // Current time in milliseconds
-
+			console.log(currentTime, expirationTime);
 			return currentTime > expirationTime; // Return true if token is expired, false otherwise
 		}
 
 		if (token && !isTokenExpired(token)) {
 			setAllowUser(true);
+			console.log("token", token);
 		} else {
+			console.log("expired", token);
+
+			localStorage.clear();
+			router.push("/login");
 			setAllowUser(false);
 		}
 	}, [allowuser]); // Empty dependency array means this effect runs only once
